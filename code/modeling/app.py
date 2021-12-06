585c0a8e7e4b8075f86ca08ed44db458bea95ebb
manu shesha <manuv3@illinois.edu>
2021-12-06 18:17:29 +0530

    Added Flask app endpoint to give details of available models

--- code/modeling/app.py
+++ code/modeling/app.py
@@ -33,21 +33,21 @@ def current_model():
 @app.route("/topics")
 def topics():
 	term_count = int(request.args.get('terms', '10'))
-	return jsonify(curr_model.get_topics(term_count = term_count))
+	return jsonify(model_objs[curr_model_idx].get_topics(term_count = term_count))
 
 @app.route("/topics/<int:topic_id>")
 def terms_for_topic(topic_id):
 	term_count = int(request.args.get('terms', '10'))
-	return jsonify(curr_model.get_topic(topic_id, term_count = term_count))
+	return jsonify(model_objs[curr_model_idx].get_topic(topic_id, term_count = term_count))
 
 @app.route("/documents")
 def documents():
-	return jsonify(curr_model.get_docs())
+	return jsonify(model_objs[curr_model_idx].get_docs())
 
 @app.route("/documents/<int:doc_id>/topics")
 def topics_for_document(doc_id):
 	term_count = int(request.args.get('terms', '10'))
 	topic_threshold = float(request.args.get('topic_threshold', '0.2'))
-	topics = curr_model.get_topics_for_doc(doc_id, topic_threshold = topic_threshold, term_count = term_count)
+	topics = model_objs[curr_model_idx].get_topics_for_doc(doc_id, topic_threshold = topic_threshold, term_count = term_count)
 	print(topics)
 	return jsonify(topics)
